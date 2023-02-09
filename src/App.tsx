70b9a80727a96ed9a8c9898e39d131b9c463be3a
minjaekang7 <minjae.kang@msystems.jp>
2023-02-09 16:11:00 +0900

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [Todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: Todos.length,
+      checked: false,
+    };
+
+    setTodos([...Todos, newTodo]);
+  };
+
+  const handleEdit = (id: number, inputValue: string) => {
+    const newTodos: Todo[] = Todos.map((todo) => {
+      if (todo.id === id) {
+        console.log(`id: ${id}, inputValue: ${inputValue}`);
+        todo.inputValue = inputValue;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const handleChecked = (id: number, inputValue: boolean) => {
+    const newTodos: Todo[] = Todos.map((todo) => {
+      if (todo.id === id) {
+        todo.checked = !inputValue;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const handleDelete = (id: number) => {
+    const newTodos: Todo[] = Todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <h2>Todo List with Typescript</h2>
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <input
+            type="text"
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            className="inputText"
+          />{" "}
+          <input type="submit" className="submitButton" />
+        </form>
+        <ul className="todoList">
+          {Todos.map((todo) => (
+            <li key={todo.id}>
+              <input
+                type="text"
+                onChange={(e) => handleEdit(todo.id, e.target.value)}
+                className="inputText"
+                value={todo.inputValue}
+                disabled={todo.checked}
+              />
+              <input
+                type="checkbox"
+                onChange={(e) => handleChecked(todo.id, todo.checked)}
+              />
+              <button
+                onClick={() => {
+                  handleDelete(todo.id);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
